4ee36249fb2fd0771b49721f40cc1344fd83251d
Slaiman Ahmadshah <sulaimanahmad321@gmail.com>
2017-04-25 15:16:00 +0100

    Adds test for filing form with player names

--- spec/feature/testing_infrastructure_spec.rb
+++ spec/feature/testing_infrastructure_spec.rb
@@ -1,6 +1,9 @@
 feature 'Testing infrastructure' do
   scenario 'Can run app and check page content' do
     visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+    fill_in :player_1_name, with: 'Unai'
+    fill_in :player_2_name, with: 'Sulaiman'
+    click_button 'Submit'
+    expect(page).to have_content 'Unai vs. Sulaiman'
   end
 end
