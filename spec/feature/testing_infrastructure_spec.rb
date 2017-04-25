7734da5ed47e9ec9395a4ff5ac108bb47d41816c
Slaiman Ahmadshah <sulaimanahmad321@gmail.com>
2017-04-25 16:27:11 +0100

    Adds view hit points test

--- spec/feature/testing_infrastructure_spec.rb
+++ spec/feature/testing_infrastructure_spec.rb
@@ -1,9 +1,17 @@
 feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
+  scenario 'Can fill in forms' do
     visit('/')
     fill_in :player_1_name, with: 'Unai'
     fill_in :player_2_name, with: 'Sulaiman'
     click_button 'Submit'
     expect(page).to have_content 'Unai vs. Sulaiman'
+  end
+
+  scenario "Can see Hit Points" do
+    visit('/')
+    fill_in :player_1_name, with: 'Unai'
+    fill_in :player_2_name, with: 'Sulaiman'
+    click_button 'Submit'
+    expect(page).to have_content 'Sulaiman: 100/100HP'
   end
 end
